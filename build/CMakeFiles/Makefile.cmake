5d56c709e7e90cba40ef0fc6eb1d0bda380bc480
shepherd-s <shepherdsoft@outlook.com>
2024-08-07 22:52:40 +0200

    Add lib folder

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -11,6 +11,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.22.1/CMakeCCompiler.cmake"
   "CMakeFiles/3.22.1/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.22.1/CMakeSystem.cmake"
+  "../test/CMakeLists.txt"
   "/usr/share/cmake-3.22/Modules/CMakeCCompiler.cmake.in"
   "/usr/share/cmake-3.22/Modules/CMakeCCompilerABI.c"
   "/usr/share/cmake-3.22/Modules/CMakeCInformation.cmake"
@@ -114,9 +115,11 @@ set(CMAKE_MAKEFILE_PRODUCTS
   "CMakeFiles/3.22.1/CMakeCCompiler.cmake"
   "CMakeFiles/3.22.1/CMakeCXXCompiler.cmake"
   "CMakeFiles/CMakeDirectoryInformation.cmake"
+  "test/CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/evlearn.dir/DependInfo.cmake"
+  "test/CMakeFiles/evlearn_test.dir/DependInfo.cmake"
   )
---
+++ build/test/CMakeFiles/evlearn_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/evlearn_test.dir/main.c.o"
+  "CMakeFiles/evlearn_test.dir/main.c.o.d"
+  "evlearn_test"
+  "evlearn_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/evlearn_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
